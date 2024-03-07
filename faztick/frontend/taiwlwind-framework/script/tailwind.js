3d81f6af80faa8d35602f31f4622446bbec64d16
Roisfaozi <roisfaozi55@gmail.com>
2024-03-08 06:21:17 +0700

    Refactore tugas

--- faztick/frontend/taiwlwind-framework/script/tailwind.js
+++ faztick/frontend/taiwlwind-framework/script/tailwind.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './**/*.{js}', './pages/*.html'],
+tailwind.config = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
